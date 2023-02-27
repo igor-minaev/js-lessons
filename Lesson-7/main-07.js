82a06309af55ee5ea5e8cb50a027fc4345a049bb
Igor Minayeu <igor.minayeu@gmail.com>
2023-02-27 23:59:50 +0300

    lesson 7

---
+++ Lesson-7/main-07.js
@@ -0,0 +1,89 @@
+//1. Реализуйте функцию, которая принимает параметром подсторку, число повторов и разделитель, а возвращает сторку, состоящую из указанного количества повторов подстроки с использованием разделителя.
+// repeatString("yo", 3, " ") => "yo yo yo"
+// repeatString("yo", 3, ",") => "yo,yo,yo"
+// for или str.repeat()
+const repeatString = (str, num, sep) => {
+    return (str + sep).repeat(num).slice(0, -1)
+}
+
+
+console.log(repeatString("yo", 3, ' '))
+console.log(repeatString("yo", 3, ','))
+
+//2. Реализуйте функцию, которая принимает параметром сторку и подстроку, а возвращает true, если строка начинается с указанной подстроки, в противном случае - false. Регистр не учитывается.
+// checkStart("Incubator", "inc") => true
+// checkStart("Incubator", "yo") => false
+// str.startWith() slice indexOF
+const checkStart = (str, subStr) => {
+    return str.toLowerCase().startsWith(subStr.toLowerCase())
+}
+
+console.log(checkStart("Incubator", "inc"))
+console.log(checkStart("Incubator", "yo"))
+
+//3. Реализуйте функцию, которая принимает параметром строку и число (количество символов), а возвращает строку из параметров, обрезанную до указанного количества символов и завершает её многоточием.
+//truncateString("Всем студентам инкубатора желаю удачи!", 10) => "Всем студе..."
+
+const truncateString = (str, num) => {
+    return `${str.slice(0, num)}...`
+}
+console.log(truncateString("Всем студентам инкубатора желаю удачи!", 10))
+
+//4. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает самое короткое слово в предложении, если в параметрах пустая строка или не строка, то возвращает null.
+// getMinLengthWord("Всем студентам инкубатора желаю удачи!") => "Всем"
+// getMinLengthWord("") => null
+// getMinLengthWord(123) => null
+// split()
+
+const getMinLengthWord = (str) => {
+    if (str === '' || typeof str !== 'string') return null
+    else return str.split(' ').reduce((a, b) => a.length < b.length ? a : b)
+}
+
+
+console.log(getMinLengthWord("Всем студентам инкубатора желаю удачи!"))
+console.log(getMinLengthWord(""))
+console.log(getMinLengthWord(123))
+
+//5. Реализуйте функцию, которая принимает параметром сторку (предложение) и возвращает то же предложение, где все слова написаны строчными, но начинаются с заглавных букв.
+// setUpperCase("всем стУдентам инкуБатора Желаю удачИ!") => "Всем Студентам Инкубатора Желаю Удачи!"
+
+const setUpperCase =(str)=>{
+    return (str.split(' ').map(w=>w[0].toUpperCase()+ w.slice(1).toLowerCase()).join(' '))
+}
+
+console.log(setUpperCase("всем стУдентам инкуБатора Желаю удачИ!"))
+
+// !!!!!!!!!!!!!!!!!!После решения 5 задач - поднимаем руку!!!!!!!!
+
+//6. Реализуйте функцию, котрая принимает параметрами строку и подстроку. Если все
+// символы подстроки содержаться в стороке - возвращает true, если нет -
+// возвращает false. Проверка проводится без учёта регистра и без учёта
+// повторяющихся символов.
+// * с учётом повторяющихся символов в подстроке
+
+
+// isIncludes("Incubator", "Cut") => true
+// isIncludes("Incubator", "table") => false
+// isIncludes("Incubator", "inbba") => true //*false
+// isIncludes("Incubator", "inba") => true  //*true
+// isIncludes("Incubator", "Incubatorrr")=> true //*false
+
+const isIncludes = (str,sub)=>{
+    const subStrInArr= sub.toLowerCase().split('')
+    const strInArr= str.toLowerCase().split('')
+    return subStrInArr.every(el=>strInArr.includes(el))
+}
+
+
+    console.log(isIncludes("Incubator", "Cut"))
+console.log(isIncludes("Incubator", "table"))
+console.log(isIncludes("Incubator", "inbba"))
+console.log(isIncludes("Incubator", "table"))
+
+
+
+
+
+
+
