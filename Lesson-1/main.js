4e5733f290c95cccef5be110f2428e56b27de684
Igor Minayeu <igor.minayeu@gmail.com>
2024-01-15 21:58:51 +0300

    js lesson 1 done

--- Lesson-1/main.js
+++ Lesson-1/main.js
@@ -54,3 +54,58 @@ const newUser = {
 }
 
 // CRUD
+
+const copyUsers = [
+    // users[0],
+    // users[1],
+    // users[2],
+    // users[3],
+    ...users,
+    newUser
+]
+
+const updatedUsers = users.map(u => u.id === 2 ? {...u, isStudent: false} : u)
+console.log(updatedUsers)
+
+const deletedUsers = updatedUsers.filter(u => u.id !== 4)
+console.log(deletedUsers)
+console.log(deletedUsers === updatedUsers)
+console.log(copyUsers === updatedUsers)
+
+const superUser = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+    }
+}
+
+const superUserCopy = {...superUser, company: {...superUser.company, catchPhrase: 'hello'}}
+
+const superUserCopy2 = {
+    ...superUser,
+    address: {
+        ...superUser.address,
+        "geo": {
+            ...superUser.address.geo, "lng": "85"
+        }
+    }
+}
+
+const superUserCopy3 = {...superUser, email: 'njdfjsdj'}
